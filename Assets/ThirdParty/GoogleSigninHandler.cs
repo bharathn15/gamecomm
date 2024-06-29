10b7502afa5658dd7ea87ca5b90d335a0a450910
bharathunity <bharath.unity15@gmail.com>
2024-06-29 20:27:53 +0530

    GoogleSigninHandler.cs file updated

--- Assets/ThirdParty/GoogleSigninHandler.cs
+++ Assets/ThirdParty/GoogleSigninHandler.cs
@@ -4,17 +4,25 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class GoogleSigninHandler : MonoBehaviour
 {
 
-
+    [SerializeField] RawImage profileImage;
+    [SerializeField] TMP_Text displayName;
+    [SerializeField] TMP_Text mail;
     [SerializeField] TMP_Text status;
 
     [SerializeField] string webClientId = "<color=blue>Web client ID</color>";
 
+    [SerializeField] Button signInButton;
+    [SerializeField] Button signOutButton;
+
 
     GoogleSignInConfiguration googleSignInConfiguration;
+    bool _userSignedIn;
 
     #region Monobehaviour callbacks
 
@@ -53,8 +61,20 @@ public class GoogleSigninHandler : MonoBehaviour
     #region Public methods
     public void SignInOnButtonClick()
     {
-        GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished);
-        
+        Task<GoogleSignInUser> task = (Task<GoogleSignInUser>)GoogleSignIn.DefaultInstance.SignIn().ContinueWith(OnAuthenticationFinished);
+        if (task.Status == TaskStatus.Faulted)
+        {
+            status.text = $"<color=red>{task.Status}</color>";
+        }
+        else if (task.Status == TaskStatus.Canceled)
+        {
+            status.text = $"<color=red>{task.Status}</color>";
+        }
+        else
+        {
+            status.text = $"<color=green>{task.Status}</color>";
+        }
+
     }
 
     public void SignInSilentlyOnButtonClick()
@@ -66,7 +86,13 @@ public class GoogleSigninHandler : MonoBehaviour
     public void SignOutOnButtonClick()
     {
         GoogleSignIn.DefaultInstance.SignOut();
+        
         status.text = string.Empty;
+        displayName.text = string.Empty;
+        mail.text = string.Empty;
+        profileImage.texture = null;
+        signInButton.gameObject.SetActive(true);
+        signOutButton.gameObject.SetActive(false);
     }
     #endregion
 
@@ -86,6 +112,8 @@ public class GoogleSigninHandler : MonoBehaviour
                 else
                 {
                     status.text = $"<color=red>Got Unexpected Exception?!? {task.Exception}</color>";
+                    _userSignedIn = true;
+                    
                 }
             }
         }
@@ -95,12 +123,41 @@ public class GoogleSigninHandler : MonoBehaviour
         }
         else
         {
+
+            displayName.text = task.Result.DisplayName;
+            mail.text = task.Result.Email;
+            status.text = $"<color=green>successfull login</color>";
             
-            status.text =   $"Given name                    {task.Result.GivenName}" +
+            StartCoroutine(GetProfilePic(task.Result.ImageUrl.ToString()));
+
+            
+            /*status.text =   $"Given name                    {task.Result.GivenName}" +
                             $"Display name                  {task.Result.DisplayName}" +
                             $"Email                         {task.Result.Email}" +
                             $"User Id                       {task.Result.UserId}" +
-                            $"ID Token                      {task.Result.IdToken}";
+                            $"ID Token                      {task.Result.IdToken}";*/
+        }
+    }
+
+
+    IEnumerator GetProfilePic(string url)
+    {
+        using(UnityWebRequest webRequest = UnityWebRequestTexture.GetTexture(url))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if(webRequest.result != UnityWebRequest.Result.Success)
+            {
+                status.text += "\nFailed to load the texture from the URL";
+            }
+            else
+            {
+                profileImage.texture = DownloadHandlerTexture.GetContent(webRequest);
+                signInButton.gameObject.SetActive(false);
+                signOutButton.gameObject.SetActive(true);
+            }
+            
+            
         }
     }
 }
